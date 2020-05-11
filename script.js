365ddd56c15c2dbb7408fa394dabb85642ddf986
Aldana <chris.bootcampmail@gmail.com>
2020-05-11 12:43:30 -0400

    the first wave of changes

---
+++ script.js
@@ -0,0 +1,24 @@
+//tasks that need completing 
+
+//make questions
+//make them interactive
+//implement a timer function
+//figure out wht to do about wrong answers
+//likely something involving booleans
+//make a function that subtracts time from the timer for every wrong answer
+//give ability to save initals and score
+ var Start= document.getElementById("start");
+ var quiz= document.getElementById("quiz")
+ var ImageQ= document.getElementById("ImageQ")
+ var question= document.getElementById("question")
+ var counter= document.getElementById("counter")
+ var timeGauge= document.getElementById("timeGauge")
+ var choiceA= document.getElementById("A")
+ var choiceB= document.getElementById("B")
+ var choiceC= document.getElementById("C")
+
+ let questions = [
+     {
+        question;"what does HTML stand for?",
+         
+     }
