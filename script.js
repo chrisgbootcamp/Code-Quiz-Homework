6fd4f1899239a3df84b987cc7904a69a4874553c
Aldana <chris.bootcampmail@gmail.com>
2020-10-03 12:44:57 -0400

    finally dispalyed high scores

--- script.js
+++ script.js
@@ -170,6 +170,11 @@ var restartBtn = document.querySelector("button.restartBtn"),
     highScores = JSON.parse(localStorage.getItem("highScores") || "[]"),
     scoreList = document.getElementById("score-list");
 
+for (var s = 0; s < highScores.length; s++) {
+  var newLi = document.createElement("li")
+  newLi.textContent = highScores[s].name + " - " + highScores[s].score
+  scoreList.appendChild(newLi)
+
     highScores.sort(function (a,b){
         return b.score - a.score
     })
